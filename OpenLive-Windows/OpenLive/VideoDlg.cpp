993f7f4d31d55eb4a4607e3bbfe7e97f0f02e4ec
sbd021 <sbd021@126.com>
2020-01-16 22:57:15 +0800

    render bug

--- OpenLive-Windows/OpenLive/VideoDlg.cpp
+++ OpenLive-Windows/OpenLive/VideoDlg.cpp
@@ -1109,14 +1109,6 @@ LRESULT CVideoDlg::OnEIDFirstLocalFrame(WPARAM wParam, LPARAM lParam)
 LRESULT CVideoDlg::OnEIDFirstRemoteFrameDecoded(WPARAM wParam, LPARAM lParam)
 {
 	LPAGE_FIRST_REMOTE_VIDEO_DECODED lpData = (LPAGE_FIRST_REMOTE_VIDEO_DECODED)wParam;
-	delete lpData;
-	lpData = NULL;
-	return false;
-}
-
-LRESULT CVideoDlg::OnEIDFirstRemoteVideoFrame(WPARAM wParam, LPARAM lParam)
-{
-	LPAGE_FIRST_REMOTE_VIDEO_FRAME lpData = (LPAGE_FIRST_REMOTE_VIDEO_FRAME)wParam;
 
 	POSITION pos = m_listWndInfo.GetHeadPosition();
 	while (pos != NULL) {
@@ -1127,15 +1119,25 @@ LRESULT CVideoDlg::OnEIDFirstRemoteVideoFrame(WPARAM wParam, LPARAM lParam)
 			break;
 		}
 	}
-	
+
 	RebindVideoWnd();
-	
+
 	SEI_INFO seiInfo;
 	memset(&seiInfo, 0, sizeof(SEI_INFO));
 	seiInfo.nUID = lpData->uid;
 	seiInfo.nWidth = lpData->width;
 	seiInfo.nHeight = lpData->height;
 	CAgoraObject::GetAgoraObject()->SetSEIInfo(seiInfo.nUID, &seiInfo);
+
+	delete lpData;
+	lpData = NULL;
+	return 0;
+	return false;
+}
+
+LRESULT CVideoDlg::OnEIDFirstRemoteVideoFrame(WPARAM wParam, LPARAM lParam)
+{
+	LPAGE_FIRST_REMOTE_VIDEO_FRAME lpData = (LPAGE_FIRST_REMOTE_VIDEO_FRAME)wParam;
 
 	delete lpData;
 	lpData = NULL;
