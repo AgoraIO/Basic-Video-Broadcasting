7b3ab273d78c1145e5e3a89f259e2740b4855b5a
sbd021 <sbd021@126.com>
2020-06-09 19:28:23 +0800

    azure qt

--- cicd/scripts/install-qt-win.cmd
+++ cicd/scripts/install-qt-win.cmd
@@ -1,4 +1,12 @@
-if exist Qt_5.10.1.7z (curl -kLO https://cdn-fastly.obsproject.com/downloads/Qt_5.10.1.7z -f --retry 5 -z Qt_5.10.1.7z) else (curl -kLO https://cdn-fastly.obsproject.com/downloads/Qt_5.10.1.7z -f --retry 5 -C -)
-7z x Qt_5.10.1.7z -oQt
-mv Qt C:\Qt5.10.1
-dir C:\Qt5.10.1
+set arch=%~1
+if %arch%==x86 (
+  if exist Qt5.14.2.zip (curl -kLO https://github.com/sbd021/Basic-Video-Broadcasting/releases/download/5.14.2/Qt5.14.2.zip -f --retry 5 -z Qt5.14.2.zip) else (curl -kLO https://github.com/sbd021/Basic-Video-Broadcasting/releases/download/5.14.2/Qt5.14.2.zip -f --retry 5 -C -)
+  7z x Qt5.14.2.zip -oQt
+  mv Qt C:\Qt5.14.2
+  dir C:\Qt5.14.2
+) else (
+ if exist Qt5.14.2_64.zip (curl -kLO https://github.com/sbd021/Basic-Video-Broadcasting/releases/download/5.14.2/Qt5.14.2_64.zip -f --retry 5 -z Qt5.14.2.zip) else (curl -kLO https://github.com/sbd021/Basic-Video-Broadcasting/releases/download/5.14.2/Qt5.14.2_64.zip -f --retry 5 -C -)
+  7z x Qt5.14.2_64.zip -oQt
+  mv Qt C:\Qt5.14.2_64
+  dir C:\Qt5.14.2_64
+)
