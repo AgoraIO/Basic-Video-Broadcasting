2a29ccb399a0718146b9cda87953f10ae44068d9
Xia Ning <xianing@agora.io>
2021-03-22 19:15:24 +0800

    use onUserJoined instead of firstRemoteVideoDecoded for setup remote video

--- OpenLive-Android/app/src/main/java/io/agora/openlive/activities/LiveActivity.java
+++ OpenLive-Android/app/src/main/java/io/agora/openlive/activities/LiveActivity.java
@@ -148,7 +148,12 @@ public class LiveActivity extends RtcBaseActivity {
 
     @Override
     public void onUserJoined(int uid, int elapsed) {
-        // Do nothing at the moment
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                renderRemoteUser(uid);
+            }
+        });
     }
 
     @Override
@@ -157,16 +162,6 @@ public class LiveActivity extends RtcBaseActivity {
             @Override
             public void run() {
                 removeRemoteUser(uid);
-            }
-        });
-    }
-
-    @Override
-    public void onFirstRemoteVideoDecoded(final int uid, int width, int height, int elapsed) {
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                renderRemoteUser(uid);
             }
         });
     }
